e2520245c62d049af2ab882a2f707bbfc3078693
ramizcildir <116086255+ramizcildir@users.noreply.github.com>
2022-11-13 11:12:03 +0300

    Add files via upload

---
+++ oto_1.hafta.py
@@ -0,0 +1,37 @@
+
+print("Bismillah") 
+
+
+
+
+
+a= 8
+b = 10
+print(a*b)
+print(b-a)
+print(a+b)
+
+
+x =5
+x=x+1  #6
+x += 1
+x *=4
+x /=2
+x **=2
+x //=3
+x %=5
+x -= 1
+print(x)
+
+
+list1= ["a", "b","c","d"]
+print(list1)
+list = list1+["r"]
+print(list)
+
+
+print(type("okul"))
+print(type(8))
+print(type(type))
+
+
