9bf04106efdc22ec45a0d68a584f5e4c1f969b56
ramizcildir <116086255+ramizcildir@users.noreply.github.com>
2022-12-17 22:57:56 +0300

    Add files via upload

---
+++ oto_9.hafta.py
@@ -0,0 +1,40 @@
+# %%
+class Araba:
+    #özellikleri
+    teker_sayisi = 4
+    renk = fiyati= "a"
+
+    
+
+z = Araba()   
+print(type(z))      # z yi araba cinsinden yazma
+
+
+
+# %%
+class toplu_tasıma():
+    #özellikleri
+    yer= "sehir"
+    def __init__(self,taksi,tren,otobüs):
+      self. taksi=taksi
+      self. tren=tren
+      self. otobüs=otobüs
+tasıma1= toplu_tasıma("sarı","4 bölme","2 kapılı")   
+tasıma2=  toplu_tasıma("uber","yük treni","3 kapılı")
+
+
+toplu_tasıma.yer   
+tasıma1.tren
+tasıma2.taksi
+
+
+# %%
+
+
+# %%
+
+
+# %%
+
+
+
